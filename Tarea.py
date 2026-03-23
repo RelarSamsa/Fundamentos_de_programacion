7e544cdf9d8623f0e5362d9c98c236b7f21bcffd
Relar <vhernandezp4@gmail.com>
2026-03-23 15:03:01 -0400

    Mi primer mensaje de guardado

---
+++ Tarea.py
@@ -0,0 +1,13 @@
+Bomba = 11
+
+while True:
+    Key = int(input("Ingrese la clave: "))
+    if Key = 1 :
+    print("Acceso concedido")
+else:
+    print("Acceso denegado")
+    Bomba = Bomba - 1
+    print(f"Detonata en {Bomba} Segundos")
+    if Bomba == 0:
+    break
+print("***** Fin del Programa c: *****")
